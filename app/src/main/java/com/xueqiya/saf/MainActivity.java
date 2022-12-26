dfcbf7d0c01c810d6a14c2efb0c8b987aa39306a
archiexue <archiexue@tencent.com>
2022-12-26 16:26:31 +0800

    feat: 添加线程切换

--- app/src/main/java/com/xueqiya/saf/MainActivity.java
+++ app/src/main/java/com/xueqiya/saf/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
@@ -20,11 +23,17 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+
         View safView = findViewById(R.id.saf);
         safView.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -40,43 +49,70 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
                     Uri uri = result.getData().getData();
                     Log.d(TAG, "uri=" + uri.toString());
-                    try {
-                        handlerFile(uri);
-                    } catch (IOException e) {
-                        Log.e(TAG, "文件处理失败:" + e);
-                        e.printStackTrace();
-                    }
+                    handlerFile(uri);
                 }
             });
 
-    private void handlerFile(Uri uri) throws IOException {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
-        if (cursor != null && cursor.moveToFirst()) {
-            int columnIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-            if (columnIndex < 0) {
-                Log.e(TAG, "获取columnIndex失败");
+    private void handlerFile(Uri uri) {
+        show();
+        Thread thread = new Thread(new FileRunnable(uri));
+        thread.start();
+    }
+
+    class FileRunnable implements Runnable {
+        private final Uri uri;
+
+        public FileRunnable(Uri uri) {
+            this.uri = uri;
+        }
+
+        @Override
+        public void run() {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                if (columnIndex < 0) {
+                    Log.e(TAG, "获取columnIndex失败");
+                    hide();
+                    cursor.close();
+                    return;
+                }
+                String displayName = cursor.getString(columnIndex);
+                Log.d(TAG, "文件名=" + displayName);
+                copyFile(uri, displayName);
+            }
+            if (cursor != null) {
                 cursor.close();
             }
-            String displayName = cursor.getString(columnIndex);
-            Log.d(TAG, "文件名=" + displayName);
-            copyFile(uri, displayName);
-        }
-        if (cursor != null) {
-            cursor.close();
         }
     }
 
-    private void copyFile(Uri uri, String displayName) throws IOException {
-        InputStream inputStream = getContentResolver().openInputStream(uri);
-        File cacheFile = new File(getCacheDir().getAbsolutePath(), displayName);
-        FileOutputStream outputStream = new FileOutputStream(cacheFile);
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = inputStream.read(buffer)) > 0) {
-            outputStream.write(buffer, 0, length);
+    private void copyFile(Uri uri, String displayName) {
+        InputStream inputStream;
+        try {
+            inputStream = getContentResolver().openInputStream(uri);
+            File cacheFile = new File(getCacheDir().getAbsolutePath(), displayName);
+            FileOutputStream outputStream = new FileOutputStream(cacheFile);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            inputStream.close();
+            outputStream.close();
+            Log.d(TAG, "拷贝完成 path=" + cacheFile);
+            hide();
+        } catch (IOException e) {
+            hide();
+            e.printStackTrace();
         }
-        inputStream.close();
-        outputStream.close();
-        Log.d(TAG, "拷贝完成 path=" + cacheFile);
+    }
+
+    private void show() {
+        handler.post(() -> progressDialog.show());
+    }
+
+    private void hide() {
+        handler.post(() -> progressDialog.hide());
     }
 }
