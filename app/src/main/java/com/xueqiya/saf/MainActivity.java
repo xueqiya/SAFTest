2e51faa5d79b8be3250c98eedef960a06c37310b
archiexue <archiexue@tencent.com>
2022-12-26 16:11:36 +0800

    uri拷贝文件至包名下

--- app/src/main/java/com/xueqiya/saf/MainActivity.java
+++ app/src/main/java/com/xueqiya/saf/MainActivity.java
@@ -13,7 +13,10 @@ import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 
-import com.xueqiya.saf.R;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
@@ -37,11 +40,16 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
                     Uri uri = result.getData().getData();
                     Log.d(TAG, "uri=" + uri.toString());
-                    handlerFile(uri);
+                    try {
+                        handlerFile(uri);
+                    } catch (IOException e) {
+                        Log.e(TAG, "文件处理失败:" + e);
+                        e.printStackTrace();
+                    }
                 }
             });
 
-    private void handlerFile(Uri uri) {
+    private void handlerFile(Uri uri) throws IOException {
         Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
         if (cursor != null && cursor.moveToFirst()) {
             int columnIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
@@ -51,9 +59,24 @@ public class MainActivity extends AppCompatActivity {
             }
             String displayName = cursor.getString(columnIndex);
             Log.d(TAG, "文件名=" + displayName);
+            copyFile(uri, displayName);
         }
         if (cursor != null) {
             cursor.close();
         }
+    }
+
+    private void copyFile(Uri uri, String displayName) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        File cacheFile = new File(getCacheDir().getAbsolutePath(), displayName);
+        FileOutputStream outputStream = new FileOutputStream(cacheFile);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        inputStream.close();
+        outputStream.close();
+        Log.d(TAG, "拷贝完成 path=" + cacheFile);
     }
 }
